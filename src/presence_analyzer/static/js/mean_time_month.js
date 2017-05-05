36af58799485fbc4c1cdb517124e204b0317b9ad
gonczor <wiktor.gonczaronek@stxnext.pl>
2017-05-05 14:48:15 +0200

    Hiding chart

--- src/presence_analyzer/static/js/mean_time_month.js
+++ src/presence_analyzer/static/js/mean_time_month.js
@@ -14,14 +14,14 @@
             loading.hide();
         });
         $('#user_id').change(function(){
-            var selected_user = $('#user_id').val(),
-                chart_div = $('#chart_div');
+            var $selectedUser = $('#user_id').val(),
+                $chartDiv = $('#chart_div');
 
-            if(selected_user) {
+            if($selectedUser) {
                 loading.show();
-                chart_div.hide();
+                $chartDiv.hide();
 
-                $.getJSON('/api/v1/mean_time_month/'+selected_user, function(result) {
+                $.getJSON('/api/v1/mean_time_month/'+$selectedUser, function(result) {
                     var data = new google.visualization.DataTable(),
                         options = {
                             hAxis: {
@@ -33,16 +33,18 @@
                             },
                         },
                         formatter = new google.visualization.DateFormat({pattern: 'HH:mm:ss'}),
-                        chart = new google.visualization.ColumnChart(chart_div[0]);
+                        chart = new google.visualization.ColumnChart($chartDiv[0]);
 
                     data.addColumn('string', 'Month');
                     data.addColumn('timeofday', 'Mean time (h:m:s)');
                     data.addRows(result);
                     formatter.format(data, 1);
-                    chart_div.show();
+                    $chartDiv.show();
                     loading.hide();
                     chart.draw(data, options);
                 });
+            } else {
+                $chartDiv.hide();
             }
         });
     });
