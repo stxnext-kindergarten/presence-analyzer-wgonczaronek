fba45206bc48852af2580f7b75a75d31be76b08a
gonczor <wiktor.gonczaronek@stxnext.pl>
2017-05-09 11:15:49 +0200

    Not found users handling: added pop-up warning.

--- src/presence_analyzer/static/js/get_avatar.js
+++ src/presence_analyzer/static/js/get_avatar.js
@@ -2,13 +2,14 @@
     $(function() {
         $('#user_id').change(function() {
             var getAvatarUrl = '/api/v1/user_avatar_url/' + $('#user_id').val(),
-                userAvatarElement = $('#user_avatar'),
+                $userAvatarElement = $('#user_avatar'),
                 intranetBaseUrl = 'https://intranet.stxnext.pl';
 
             $.getJSON(getAvatarUrl, function(result) {
-                userAvatarElement.attr('src', intranetBaseUrl + result);
+                $userAvatarElement.attr('src', intranetBaseUrl + result);
             }).fail(function() {
-                userAvatarElement.attr('src', '');
+                $userAvatarElement.attr('src', '');
+                window.alert('User details not found.');
             });
         });
     });
